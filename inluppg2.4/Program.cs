02c20ef35cd90a6fcfa460f67dd53cc554ffb0a8
emsi06001 <emsi06001@E-PW0234N3>
2023-09-13 13:22:00 +0200

    Add project files.

---
+++ inluppg2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace inluppg2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lön nummer 1:");
+            int lon1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lön nummer 2:");
+            int lon2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lön nummer 3:");
+            int lon3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Medellönen är {(lon1 + lon2 + lon3) / 3}");
+        }
+    }
+}
